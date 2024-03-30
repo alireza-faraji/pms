a1a80b5ad379ddf5683c4188bc47dd65e21d899a
Alireza <eng.alirezafaraji@gmail.com>
2024-03-30 10:27:16 +0330

    update

---
+++ pms/tender/doctype/pms_quotation_approve/test_pms_quotation_approve.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Rayan Soft Land and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPMSQuotationApprove(FrappeTestCase):
+	pass
---
+++ pms/tender/doctype/pms_qutation_reveiw/pms_qutation_reveiw.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Rayan Soft Land and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PMS Qutation Reveiw', {
+	// refresh: function(frm) {
+
+	// }
+});
